df317b1c6a354c6c85fea9e93f229858172dc808
Ali Bouzar <alibouzar@foi.hr>
2021-03-06 18:43:30 +0100

    first commit

---
+++ src/main/java/com/miaoubich/HibernateTutorial/SpringbootThymeleafCrudWebappApplication.java
@@ -0,0 +1,13 @@
+package com.miaoubich.HibernateTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafCrudWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafCrudWebappApplication.class, args);
+	}
+
+}
